99a6cc1c293135dbe21ff2c0887f29e2fd9c3369
Princess Ysabela Eudela <pyeudela@gmail.com>
2025-05-27 20:57:05 +0400

    Update MenuList.tsx

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
 
-
+export const dynamic = "force-dynamic";
 const MenuList = async () => {
   const wixClient = await wixClientServer();
 
